326e5d0b1521d4f3aa423731ff27d10423c54428
quocdatit2k10 <quocdatit2k10@gmail.com>
2016-04-07 23:01:41 +0900

    asdfgh

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -2,12 +2,12 @@
 
 define('_PS_CACHING_SYSTEM_', 'CacheFs');
 define('_PS_CACHE_ENABLED_', '1');
-define('_DB_NAME_', 'ngoinhat_oiyeu');
+define('_DB_NAME_', 'heronguyen');
 define('_MYSQL_ENGINE_', 'InnoDB');
-define('_DB_SERVER_', '127.0.0.1');
-define('_DB_USER_', 'ngoinhat_oiyeu');
+define('_DB_SERVER_', 'heronguyen.cdmngv7j0t1y.ap-southeast-1.rds.amazonaws.com');
+define('_DB_USER_', 'heronguyen');
 define('_DB_PREFIX_', 'ps_');
-define('_DB_PASSWD_', 'ngoinhatoiyeu');
+define('_DB_PASSWD_', 'ko761311');
 define('_COOKIE_KEY_', 'uz8GPQQ8wMb1onvEYZ2YMaixXDX2VnSIy5kHVtTGVDwrhdrDmHDW28UP');
 define('_COOKIE_IV_', 'ARX3ENDo');
 define('_PS_CREATION_DATE_', '2015-12-13');
