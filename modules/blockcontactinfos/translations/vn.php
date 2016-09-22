33652efa6303dc59e8fda109243a06ec61ed5536
quocdatit2k10 <quocdatit2k10@gmail.com>
2016-09-22 17:30:14 +0900

    [Ngoi nha toi yeu] sua thanh [Japan Domestic]

--- modules/blockcontactinfos/translations/vn.php
+++ modules/blockcontactinfos/translations/vn.php
@@ -15,7 +15,7 @@ $_MODULE['<{blockcontactinfos}prestashop>blockcontactinfos_c9cc8cce247e49bae79f1
 $_MODULE['<{blockcontactinfos}prestashop>blockcontactinfos_02d4482d332e1aef3437cd61c9bcc624'] = 'Liên hệ với chúng tôi';
 $_MODULE['<{blockcontactinfos}prestashop>blockcontactinfos_2e006b735fbd916d8ab26978ae6714d4'] = 'Số điện thoại';
 $_MODULE['<{blockcontactinfos}prestashop>blockcontactinfos_6a1e265f92087bb6dd18194833fe946b'] = 'Hộp thư';
-$_MODULE['<{blockcontactinfos}prestashop>blockcontactinfos_80a11d2a54a677f6fadd9c041c0d6b98'] = 'Ngôi nhà tôi yêu';
+$_MODULE['<{blockcontactinfos}prestashop>blockcontactinfos_80a11d2a54a677f6fadd9c041c0d6b98'] = 'Japan Domestic';
 $_MODULE['<{blockcontactinfos}prestashop>blockcontactinfos_320abee94a07e976991e4df0d4afb319'] = 'Hãy gọi cho chúng tôi:';
 
 
