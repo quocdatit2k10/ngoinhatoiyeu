db5cfb3c502cf712a0bacfdf8c869e2112d7b6c0
quocdatit2k10 <quocdatit2k10@gmail.com>
2016-10-24 22:28:08 +0900

    chinh test phan gui mail ngoai trang home

--- modules/blocknewsletter/translations/vn.php
+++ modules/blocknewsletter/translations/vn.php
@@ -15,8 +15,8 @@ $_MODULE['<{blocknewsletter}prestashop>blocknewsletter_77c576a354d5d6f5e2c1ba501
 $_MODULE['<{blocknewsletter}prestashop>blocknewsletter_506e58042922bff5bd753dc612e84f5b'] = 'Đến với mã chứng từ';
 $_MODULE['<{blocknewsletter}prestashop>blocknewsletter_1d612b943b8f4792bff603384a46e5f1'] = 'Để trống để vô hiệu hoá ';
 $_MODULE['<{blocknewsletter}prestashop>blocknewsletter_13348442cc6a27032d2b4aa28b75a5d3'] = 'Tìm kiếm';
-$_MODULE['<{blocknewsletter}prestashop>blocknewsletter_ffb7e666a70151215b4c55c6268d7d72'] = 'E-mail';
-$_MODULE['<{blocknewsletter}prestashop>blocknewsletter_416f61a2ce16586f8289d41117a2554e'] = 'E-mail của bạn';
+$_MODULE['<{blocknewsletter}prestashop>blocknewsletter_ffb7e666a70151215b4c55c6268d7d72'] = 'Đăng ký nhận Tin tức';
+$_MODULE['<{blocknewsletter}prestashop>blocknewsletter_416f61a2ce16586f8289d41117a2554e'] = 'Hãy nhập email của bạn';
 $_MODULE['<{blocknewsletter}prestashop>blocknewsletter_d8335f4a5d918134bd0bdc47cc487d74'] = 'Nhập e-mail của bạn';
 $_MODULE['<{blocknewsletter}prestashop>blocknewsletter_590b7aefc83952e3483c5168bf93e19f'] = 'Thông báo : %1$s';
 
